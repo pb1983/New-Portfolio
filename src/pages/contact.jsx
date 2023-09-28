4a1997013a0441dccb6949d9f394294acea3328d
pb1983 <phil.bryer@gmail.com>
2023-09-27 20:08:26 -0400

    Finalized styling

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,14 +4,16 @@ export default function Contact() {
 
 
   return (
-        <div className="col-8 mt-2 m-2">
-          <h3>Contact</h3>
-          <p>
-            If you're interested in contacting me, please fill out the form below. I check my messages often and will get back to you ASAP. 
+    <div className="container">
+      <div className="row justify-content-center pt-4">
+        <div className="col-12 row justify-content-center pt-4">
+          <h3 className="text-center pt-4 pb-2">Contact</h3>
+          <p className="text-center pb-2">
+            If you're interested in contacting me, please fill out the form below. I check my messages often and will get back to you ASAP.
           </p>
-          
           <Form></Form>
-
-          </div>
-      );
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,29 +1,38 @@
 import MyResume from '../assets/Phil_Bryer_Resume.pdf'
 
 export default function Resume() {
-    return (
-        <div className="mt-2 m-2">
-          <h3>Resume</h3>
-          <p>
+  
+  return (
+    <div className="container mt-4 pt-4 m-2">
+      <div className="row justify-content-center text-center">
+        <div className="col-md-12 mt-4">
+          <h3 className='mt-4'>Resume</h3>
+          <p className='pt-2 pb-2'>
             If you would like to see more of my credentials and previous experience, download my <u><a href={MyResume} download>resume</a></u>.
           </p>
+        </div>
+        <div className="col-md-2">
           <h5>Front End Skills:</h5>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Javascript</li>
-            <li>Bootstrap</li>
-            <li>Bulma</li>
-            <li>React</li>
-          </ul>
-          <h5>Server-Side Skills:</h5>
-          <ul>
-            <li>Node.js</li>
-            <li>Express</li>
-            <li>MySQL</li>
-            <li>MongoDB</li>
-            <li>API routing</li>
+          <ul className='list-group'>
+            <li className='list-group-item'>HTML</li>
+            <li className='list-group-item'>CSS</li>
+            <li className='list-group-item'>Javascript</li>
+            <li className='list-group-item'>Bootstrap</li>
+            <li className='list-group-item'>Bulma</li>
+            <li className='list-group-item'>React</li>
           </ul>
         </div>
-      );
+        <div className="col-md-2">
+          <h5>Server-Side Skills:</h5>
+          <ul className='list-group'>
+            <li className='list-group-item'>Node.js</li>
+            <li className='list-group-item'>Express</li>
+            <li className='list-group-item'>MySQL</li>
+            <li className='list-group-item'>MongoDB</li>
+            <li className='list-group-item'>API routing</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
